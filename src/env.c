26d4b436a4e4845f8055f3af6bf0912bf1477681
asqel <axel.lleres@gmail.com>
2025-05-01 19:28:55 +0200

    set_env/get_env/unset_env

--- src/env.c
+++ src/env.c
@@ -6,7 +6,7 @@
 /*   By: axlleres <axlleres@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 20:29:49 by axlleres          #+#    #+#             */
-/*   Updated: 2025/04/10 19:22:15 by axlleres         ###   ########.fr       */
+/*   Updated: 2025/05/01 19:28:43 by axlleres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ char **msh_build_env(t_msh_ctx *ctx)
 }
 
 // exists can be NULL
-// doesnt return a copy 
+// doesnt return a copy
 char *msh_get_env(t_msh_ctx *ctx, char *name, int *exists)
 {
 	int	i;
@@ -54,3 +54,48 @@ char *msh_get_env(t_msh_ctx *ctx, char *name, int *exists)
 	return (NULL);
 }
 
+void msh_set_env(t_msh_ctx *ctx, char *name, char *value)
+{
+	int	i;
+	t_msh_env_var_t	*new_env;
+
+	i = -1;
+	while (++i < ctx->env_len)
+		if (!ft_strcmp(ctx->env[i].name, name))
+		{
+			free(ctx->env[i].value);
+			ctx->env[i].value = ft_strdup(value);
+			return ;
+		}
+	new_env = malloc(sizeof(t_msh_env_var_t) * (ctx->env_len + 1));
+	if (!new_env)
+		return ;
+	new_env[ctx->env_len].name = ft_strdup(name);
+	new_env[ctx->env_len].value = ft_strdup(value);
+	ctx->env_len++;
+	i = -1;
+	while (++i < ctx->env_len - 1)
+		new_env[i] = ctx->env[i];
+	free(ctx->env);
+	ctx->env = new_env;
+}
+
+void msh_unset_env(t_msh_ctx *ctx, char *name)
+{
+	int	i;
+
+	i = -1;
+	while (++i < ctx->env_len)
+		if (!ft_strcmp(ctx->env[i].name, name))
+		{
+			free(ctx->env[i].name);
+			free(ctx->env[i].value);
+			while (i < ctx->env_len - 1)
+			{
+				ctx->env[i] = ctx->env[i + 1];
+				i++;
+			}
+			ctx->env_len--;
+			return ;
+		}
+}
